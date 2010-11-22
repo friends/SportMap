64f8a84073cec4c6254797c80e64dc88f1555555
yanghaichuan <yanghaichuan@live.cn>
2010-11-22 22:44:40 +0800

    web.config change

---
+++ App_Code/IHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportMap
+{
+    enum ErrorMessage
+    {
+        OK
+    }
+    /// <summary>
+    ///操作数据库中表的增删改操作
+    /// </summary>
+    public interface IHandler
+    {
+        ErrorMessage Insert(object o);
+        ErrorMessage Delete(object o);
+        ErrorMessage Modify(object o);
+        ErrorMessage Submit(object o);
+    }
+}
